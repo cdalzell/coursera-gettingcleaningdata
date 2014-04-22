ab5150c4af8ed544aa7023b1d78b55cc728d4c23
Chris Dalzell <cdalzell@gmail.com>
2014-04-21 20:16:42 -0400

    Step #1: Read and merge the test and training data sets

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,23 @@
 # working directory that does the following:
 
 # 1. Merges the training and the test sets to create one data set.
+print("Loading X Data..")
+
+xTest <- read.table("test/X_Test.txt")
+xTrain <- read.table("train/X_train.txt")
+xData <- rbind(xTest, xTrain)
+
+print("Loading Y Data..")
+
+yTest <- read.table("test/Y_Test.txt")
+yTrain <- read.table("train/y_train.txt")
+yData <- rbind(yTest, yTrain)
+
+print("Loading Subject Data..")
+
+subjectTest <- read.table("test/subject_test.txt")
+subjectTrain <- read.table("train/subject_train.txt")
+subjectData <- rbind(subjectTest, subjectTrain)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
@@ -10,3 +27,4 @@
 # 4. Appropriately labels the data set with descriptive activity names.
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
